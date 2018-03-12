58529bf97543dbf897cf2f70428664cdb98b30ae
Naman Aggarwal <naman@Namans-MacBook-Pro.local>
2018-03-13 01:22:55 +0530

    Modified the code

--- nbody1.cpp
+++ nbody1.cpp
@@ -1,44 +1,96 @@
 #include<iostream>
+#include<stdlib.h>
+#include<math.h>
 using namespace std;
 
 const double G = 0.000000000066741;  //True value of .0000000000667408 rounded off to 15 decimal places (precision of double)
 const double M1 = 1; //for present analysis
 const double M2 = 1; //for present analysis
+const double timestep = 0.025; // small time step in seconds
 
-// returns force along the x-axis on the specified body//
-double Fx(double r,double X1, double X2, int body){
-	// r is the distance between the two bodies
-	double F = (G*(X2-X1))/(r*r*r) ;
-	if(body == 1){
-		return F;
+struct Body{ 
+	double M;
+	double r[2], v[2], a[2]; // defines acceleration of the body in the x and y directions; a[0] = a_x, a[1] = a_y; and likewise for r and v
+	
+	/* vector<double>* acceleration(Body body){
+		//returns a pointer to the acceleration vector
+		double r = sqrt((body.X - X)*(body.X -X) + (body.Y - Y)*(body.Y - Y));
+		new vector<double> F;
+		double F
+		F.push_back()
+
+	} */
+	void acceleration_update(Body body){
+		double r_ = sqrt((body.r[0] - r[0])*(body.r[0] -r[0]) + (body.r[1] - r[1])*(body.r[1] - r[1]));
+		a[0] = G*(body.r[0] - r[0])/(r_*r_*r_);
+		a[1] = G*(body.r[1] - r[1])/(r_*r_*r_);
+		return;
 	}
-	else return -F;
-}
 
-//returns force along the y-xis on the specified body//
-double Fy(double r,double Y1, double Y2, int body){
-	// r is the distance between the two bodies
-	double F = (G*(Y2-Y1))/(r*r*r) ;
-	if(body == 1){
-		return F;
+	void r_update(Body body){
+		r[0] = r[0] + v[0]*timestep + (1/2)*a[0]*timestep*timestep ;
+		r[1] = r[1] + v[1]*timestep + (1/2)*a[1]*timestep*timestep ;
+		return;
 	}
-	else return -F;
-}
-// NOTE : Forces also represent accelerations as masses are 1 unit //
+	void v_update(Body body){
+		v[0] = v[0] + (1/2)*a[0]*timestep;
+		v[1] = v[1] + (1/2)*a[1]*timestep;
 
-//you would like to make four vectors corresponding to position and the velocity of the two bodies//
-
-//creating a function to update X1//
-
-double X_new(double timestep, ){
-
-}
-
+		(*this).acceleration_update(body); // now, a_present = a_(i+1)
+		v[0] = v[0] + (1/2)*a[0]*timestep;
+		v[1] = v[1] + (1/2)*a[1]*timestep;
+		return;
+	}
+};
 int main(){
 	// all the physical properties are in S.I. units
 	// 6N equations for N bodies
 	// Xj_o, Yj_o, Uj_o, Vj_o are known for j = 1 , 2 
 
-	//* LEAPFROG INTEGRAL *//
+	// LEAPFROG INTEGRAL //
+	Body body1, body2;
+	body1.M = 1; body2.M = 1;
+	body1.r[0] = 0; body1.r[1] = 0; body1.v[0] = 0; body1.v[1] = 0;
+	body2.r[0] = 5; body2.r[1] = 0; body2.v[0] = 0; body2.v[1] = 0;
+	body1.acceleration_update(body2);
+	body2.acceleration_update(body1); 
+	//sets up initial accelerations of the bodies
 
+	double duration; //in seconds
+	cin>>duration;
+
+	int h = duration/timestep; // h =number of iterations
+	double trajectory_body_x[2*h]; //first h terms are X coordinate of body 1 and the rest of the terms are for body 2
+	double trajectory_body_y[2*h]; //first h terms are Y coordinate of body 1 and the rest of the terms are for body 2
+	
+	trajectory_body_x[0] = body1.r[0];
+	trajectory_body_x[h] = body2.r[0];
+	trajectory_body_y[0] = body1.r[1];
+	trajectory_body_y[h] = body2.r[1]; 
+
+	for(int i=1; i<2*h; i++){
+		body1.r_update(body2);
+		body1.v_update(body2);
+
+		body2.r_update(body1);
+		body2.v_update(body1);
+
+		//storing the trajectory//
+
+		trajectory_body_x[i] = body1.r[0];
+		trajectory_body_x[i+h] = body2.r[0];
+		trajectory_body_y[i] = body1.r[1];
+		trajectory_body_y[i+h] = body2.r[1];
+	}
+
+	for (int i = 0; i < h; i++)
+	{
+		cout<<trajectory_body_x[i]<<" "<<trajectory_body_y[i]<<endl;
+	}
+	cout<<endl;
+	cout<<"For Body 2:"<<endl;
+	for (int i = 0; i < h; i++)
+	{
+		cout<<trajectory_body_x[i+h]<<" "<<trajectory_body_y[i+h]<<endl;
+	}
 }
