2c62c4f7b3aa72d5efd15cb1487d0fa981330f0c
krishnaw14 <krishnaw14@gmail.com>
2018-03-29 19:56:15 +0530

    Working code- very crude

--- n_body.cpp
+++ n_body.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
+#include <vector>
 #include <string> 
 #include <fstream> //enables export to csv files
 //#include"twobodystruct.h"
@@ -12,7 +13,8 @@ const long long int M1 = 1000; //for present analysis
 const long long int M2 = 2000; //for present analysis
 const double timestep = 0.05; // small time step in seconds
 
-struct Body{ 
+struct Body
+{ 
 	double M;
 	double r[2], v[2], a[2]; // defines acceleration of the body in the x and y directions; a[0] = a_x, a[1] = a_y; and likewise for r and v
 	
@@ -30,12 +32,14 @@ struct Body{
 		v[1] = vel_y;
 	}
 	
-	void acceleration_update( Body body_array[], int n)
+	void acceleration_update( vector<Body> body_array, int n)
 	{
 		for (int i=0;i<n;i++)
 		{
 			Body body=body_array[i];
 			double r_ = sqrt((body.r[0] - r[0])*(body.r[0] -r[0]) + (body.r[1] - r[1])*(body.r[1] - r[1]));
+			if(r_==0)
+				continue;
 			a[0] += G*(body.M)*(body.r[0] - r[0])/(r_*r_*r_);
 			a[1] += G*(body.M)*(body.r[1] - r[1])/(r_*r_*r_);
 
@@ -49,7 +53,7 @@ struct Body{
 		r[1] = r[1] + v[1]*timestep + (1.0/2)*a[1]*timestep*timestep ;
 	}
 
-	void v_update(Body body_array[], int n){
+	void v_update(vector<Body> body_array, int n){
 		v[0] = v[0] + (1.0/2)*a[0]*timestep;
 		v[1] = v[1] + (1.0/2)*a[1]*timestep;
 
@@ -63,20 +67,28 @@ struct Body{
 	{
 		cout<<"x = "<<r[0]<<" y = "<<r[1]<<endl;
 	}
+
 };
 
 int main()
 {
 	int n=5;
-	Body body_array[n]; //Array of bodies. Aim is to determine the trajectories of each body stored in body_array
+	vector<Body> body_array;
+	//Body body_array[n]; //Array of bodies. Aim is to determine the trajectories of each body stored in body_array
 
 	//Initialize position, velocity and mass of each body
 
-	body_array[0](M1, 0, 0, 100, 0);
-	body_array[1](2*M1,5, 0, -100, 0);
-	body_array[2](3*M1, 10, 0, 200, 50);
-	body_array[3](4*M1, 15, 0, 300, -350);
-	body_array[4](5*M1, 20, 0, 400, 800);
+	Body body1(1*M1, 0, 0, 100, 0);
+	Body body2(2*M1,5, 0, -100, 0);
+	Body body3(3*M1, 10, 0, 200, 50);
+	Body body4(4*M1, 15, 0, 300, -350);
+	Body body5(5*M1, 20, 0, 400, 800);
+
+	body_array.push_back(body1);
+	body_array.push_back(body2);
+	body_array.push_back(body3);
+	body_array.push_back(body4);
+	body_array.push_back(body5);
 
 	for (int i=0;i<n;i++)
 		body_array[i].acceleration_update(body_array, n); //sets up initial accelerations of the bodies
@@ -100,7 +112,7 @@ int main()
 			body_array[j].r_update();
 			body_array[j].v_update(body_array, n);
 
-			cout<<"Body"+to_string(j);
+			cout<<"Body "+to_string(j);
 		    body_array[j].print_r();
 		}
 
