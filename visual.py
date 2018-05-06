4ff7100a693fdf0d58ddfe7617a43b7c96067f71
abhay <abhayjindal61@gmail.com>
2018-05-06 23:13:45 +0530

    Visualization code for general n boy

--- visual.py
+++ visual.py
@@ -3,14 +3,18 @@ import time
 from pygame.locals import *
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
-color = (0,40,40)
+color = (250,250,250)
 height = 1000
 width = 1000
 screen = pygame.display.set_mode((width,height))
-pygame.display.set_caption('N-body visualization')
+pygame.display.set_caption('N-body simulations')
 screen.fill(color)
 clock = pygame.time.Clock()
-n = 3840
+n = 0
+N = 128
+timestep = 0.05
+t = 10
+
 F = open("output.txt","r")
 coor = []
 for line in F.readlines():
@@ -19,14 +23,14 @@ for line in F.readlines():
     x[1] = int(x[1]*100 + 300)
     coor.append(x)
 
-while True:
+while (n < (N*10)/timestep):
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     screen.fill(color)
-    for i in range(128):
-        pygame.draw.circle(screen, (255,255,0), coor[n+i], 5)
-    #n += 128
+    for i in range(N):
+        pygame.draw.circle(screen, (0,0,255), coor[n+i], 5)
+    n += N
     time.sleep(0.05)
     pygame.display.update()
